3e5896cb520516b9cf8f07c66bb3886984d7c71a
limyt0 <80087763+limyt0@users.noreply.github.com>
2026-01-31 08:27:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    int n1, n2;
+    int min, max;
+    if (n < m) {
+        n1 = n; n2 = m;
+    }
+    else {
+        n1 = m; n2 = n;
+    }
+    for (int i = 1; i <= n1; i++)
+    {
+       if (n2 % i == 0 && n1 % i == 0) max = i;
+        
+    }
+    min = (n2 / max) * n1;
+    answer.push_back(max);
+    answer.push_back(min);
+    return answer;
+}
