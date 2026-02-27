4b216ed6fca5b6966666228b467eb7e0f628e1ac
limyt0 <dudx45@gmail.com>
2026-02-27 09:52:00 +0900

    test

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int solution(int num1, int num2) {
-    
-    return (num1 * 1000 ) / num2;
-}
--- 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int solution(int age) {
-    
-    return 2022 - age + 1;
-}
